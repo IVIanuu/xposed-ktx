99efa672203e15ea0d8153c10eea78f339533bb2
IVIanuu <IVIanuu@gmail.com>
2017-10-26 00:54:55 +0200

    Added new hook methods
    Fixed hooking from classloader

--- xposedextensions/src/main/java/com/ivianuu/xposedextensions/Hooks.kt
+++ xposedextensions/src/main/java/com/ivianuu/xposedextensions/Hooks.kt
@@ -34,34 +34,67 @@ class MethodHook {
     private var before: ((Param) -> Unit)? = null
     private var after: ((Param) -> Unit)? = null
     private var replace: ((Param) -> Any?)? = null
-    
+    private var returnConstant: Any? = null
+
+    private var beforeSet = false
+    private var afterSet = false
+    private var replaceSet = false
+    private var returnConstantSet = false
+    private var doNothingSet = false
+
+    /**
+     * Sets the priority
+     */
     fun priority(priority: Int) {
         this.priority = priority
     }
 
     fun priority(action: () -> Int) {
-        this.priority = action()
+        priority(action())
     }
 
     fun before(action: (Param) -> Unit) {
         this.before = action
+        this.beforeSet = true
     }
 
     fun after(action: (Param) -> Unit) {
         this.after = action
+        this.afterSet = true
     }
 
     fun replace(action: (Param) -> Any?) {
         this.replace = action
+        this.replaceSet = true
     }
 
-    internal fun build(): XC_MethodHook {
-        return if (replace != null) {
+    fun returnConstant(constant: Any?) {
+        this.returnConstant = constant
+        this.returnConstantSet = true
+    }
+
+    fun returnConstant(action: () -> Any?) {
+        returnConstant(action())
+    }
+
+    fun doNothing() {
+        doNothingSet = true
+    }
+
+    fun doNothing(action: () -> Unit) {
+        doNothingSet = true
+    }
+
+    internal fun build(): XC_MethodHook = when {
+        doNothingSet -> XC_MethodReplacement.DO_NOTHING
+        returnConstantSet -> XC_MethodReplacement.returnConstant(priority, returnConstant)
+        replaceSet -> {
             object : XC_MethodReplacement() {
                 override fun replaceHookedMethod(param: MethodHookParam): Any? =
                         replace?.invoke(Param(param))
             }
-        } else {
+        }
+        beforeSet || afterSet -> {
             object : XC_MethodHook() {
                 override fun beforeHookedMethod(param: MethodHookParam) {
                     before?.let { it(Param(param)) }
@@ -71,6 +104,7 @@ class MethodHook {
                 }
             }
         }
+        else -> throw IllegalArgumentException("invalid configuration")
     }
 }
 
@@ -136,15 +170,34 @@ fun Class<*>.hook(methodName: String = "",
  */
 fun ClassLoader.hook(className: String,
                      methodName: String = "",
-                     vararg args: Any,
                      init: MethodHook.() -> Unit): Unhook {
     val hook = MethodHook()
     init(hook)
     return if (methodName.isEmpty()) {
-        XposedHelpers.findAndHookMethod(
-                className, this, methodName, *args, hook.build())
-    } else {
         XposedHelpers.findAndHookConstructor(
+                className, this, methodName, hook.build())
+    } else {
+        XposedHelpers.findAndHookMethod(
+                className, this, methodName, hook.build())
+    }
+}
+
+/**
+ * Hooks all methods with name
+ * If the name is empty it will the constructors
+ */
+fun ClassLoader.hook(className: String,
+                     methodName: String = "",
+                     vararg args: Any,
+                     init: MethodHook.() -> Unit): Unhook {
+    val hook = MethodHook()
+    init(hook)
+
+    return if (methodName.isEmpty()) {
+        XposedHelpers.findAndHookConstructor(
+                className, this, *args, hook.build())
+    } else {
+        XposedHelpers.findAndHookMethod(
                 className, this, methodName, *args, hook.build())
     }
 }

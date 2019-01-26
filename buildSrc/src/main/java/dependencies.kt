a94a8d3e56a3810015648e1b20a3c4078c53cb60
IVIanuu <IVIanuu@gmail.com>
2019-01-26 16:56:47 +0100

    Migrated to bintray

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -11,10 +11,18 @@ object Build {
     const val versionName = "0.0.1"
 }
 
+object Publishing {
+    const val groupId = "com.ivianuu.xposedktx"
+    const val vcsUrl = "https://github.com/IVIanuu/xposed-ktx"
+    const val version = "git"
+}
+
 object Versions {
     const val androidGradlePlugin = "3.3.0"
 
     const val androidxAppCompat = "1.0.2"
+
+    const val bintray = "1.8.4"
 
     const val kotlin = "1.3.20"
     const val mavenGradlePlugin = "2.1"
@@ -25,6 +33,9 @@ object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
 
     const val androidxAppCompat = "androidx.appcompat:appcompat:${Versions.androidxAppCompat}"
+
+    const val bintrayGradlePlugin =
+        "com.jfrog.bintray.gradle:gradle-bintray-plugin:${Versions.bintray}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
